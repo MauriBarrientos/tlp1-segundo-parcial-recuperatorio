4ffde93b9e7b553e5b63e72b88b4cbd22002e0c5
MauriBarrientos <mauribarrientos23@gmail.com>
2023-07-01 15:33:01 -0300

    Configuración de servidor en puerto 4000, instalación de otras dependencias

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 // Imports
 const cors = require('cors');
 const express = require('express');
-
 const path = require('path');
-
+const morgan = require('morgan');
 const app = express();
+require('dotenv').config();
+require('ejs');
 
 // Middlewares
-// TODO: Implementar middlewares
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
+//Instancia de conexion a la base de datos
+
+//Instancia de conexion con el puerto
+const port = process.env.PORT || 4000;
+
+//Archivos estáticos usando la librería path de NodeJS
 app.use(express.static(path.join(__dirname, 'public')));
+//Configurar el motor de plantitllas EJS
+app.set('view engine', 'ejs');
 
 // Routes
 app.use('/api', require('./routes/reserva.routes'));
@@ -17,4 +29,4 @@ app.use('/api', require('./routes/reserva.routes'));
 // TODO: Si la petición no coincide con ninguna de las rutas declaradas, mostrar error 404
 
 // Starting the server
-app.listen(45635, () => console.log('Server on port xxxx'));
+app.listen(4000, () => console.log(`Server on port ${port}`));
