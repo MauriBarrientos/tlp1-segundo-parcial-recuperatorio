48bc76ef233606bd5bd8ecd749cb66a4a422964d
MauriBarrientos <mauribarrientos23@gmail.com>
2023-07-01 15:56:17 -0300

    Creacion del modelo de datos

--- models/Reserva.js
+++ models/Reserva.js
@@ -1,1 +1,60 @@
-// TODO: Crear modelo de datos de Reserva
+const { sequelize, DataTypes } = require('../database');
+
+const Reserva = sequelize.define('Reserva', {
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoincrement: true,
+    },
+
+    nombre: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    apellido: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    fechaEntrada: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    fechaSalida: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    numero: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    estado: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
+    },
+    updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
+    },
+    deletedAt: {
+        type: DataTypes.DATE,
+        allowNull: true
+    }
+}, {
+    // Other model options go here
+    createdAt: true,
+    updatedAt: true,
+    deletedAt: true,
+    tableName: 'reservadb'
+});
+
+// Crear tabla si no existe
+Reserva.sync();
+
+module.exports = Reserva;
